0bbdcba8fbf8c5ee143099a89769bcfe6f55c20f
Demosthen <1008981@gmail.com>
2022-07-26 22:10:51 -0700

    hack some wind into the gridworld env

--- run_gridworld.py
+++ run_gridworld.py
@@ -1,5 +1,6 @@
 import gym 
-import gym_simplegrid
+from gym_simplegrid.envs import simple_grid
+import matplotlib.pyplot as plt
 import argparse
 
 """SimpleGrid is a super simple gridworld environment for OpenAI gym. It is easy to use and customise and it is intended to offer an environment for quick testing and prototyping different RL algorithms.
@@ -10,14 +11,16 @@ It is also efficient, lightweight and has few dependencies (gym, numpy, matplotl
 
 SimpleGrid involves navigating a grid from Start(S) (red tile) to Goal(G) (green tile) without colliding with any Wall(W) (black tiles) by walking over the Empty(E) (white tiles) cells. The yellow circle denotes the agent's current position.
 
+
 Optionally, it is possible to introduce a noise in the environment that makes the agent move in a random direction that can be different than the desired one."""
 def read_gridworld(filename):
     with open(filename, 'r') as f:
         grid_str = f.read()
-        grid, rew = grid_str.split("---")
+        grid, rew, wind_p = grid_str.split("---")
         grid_desc = eval(grid)
         rew_map = eval(rew)
-    return grid_desc, rew_map
+        wind_p = eval(wind_p)
+    return grid_desc, rew_map, wind_p
 
 def add_args(parser):
     parser.add_argument(
@@ -31,9 +34,10 @@ if __name__=="__main__":
     add_args(parser)
     args = parser.parse_args()
 
-    grid_desc, rew_map = read_gridworld(args.filename)
-    env = gym.make('SimpleGrid-v0', desc=grid_desc, reward_map = rew_map)
+    grid_desc, rew_map, wind_p = read_gridworld(args.filename)
+    env = gym.make('SimpleGrid-v0', desc=grid_desc, reward_map = rew_map, wind_p = wind_p)
     env.reset()
     pic = env.render(mode="ansi")
     print(pic)
+    # plt.imsave("test.png", pic)
     
