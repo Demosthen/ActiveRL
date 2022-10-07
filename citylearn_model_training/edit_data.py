94b22529f3726f49e1093409e882284770d71120
Demosthen <1008981@gmail.com>
2022-10-07 12:45:22 -0700

    update edit_data.py

--- citylearn_model_training/edit_data.py
+++ citylearn_model_training/edit_data.py
@@ -9,9 +9,17 @@ if __name__ == "__main__":
     new_store = pd.HDFStore("citylearn_model_training/planning_model_data_new.h5")
     num_rows = num_episodes * 8760
     obs_df = data.select("obs", stop=num_rows)
-    action_df = data.select("actions", stop=num_rows)
-    next_obs_df = data.select("next_obs", stop=num_rows)
-    print(obs_df.shape)
     new_store.put("obs", obs_df)
+    del obs_df
+    
+    action_df = data.select("actions", stop=num_rows)
     new_store.put("action", action_df)
+    del action_df
+    next_obs_df = data.select("next_obs", stop=num_rows)
     new_store.put("next_obs", next_obs_df)
+    del next_obs_df
+    data.close()
+    print(obs_df.shape)
+    
+    
+    
