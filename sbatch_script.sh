1baf3fde75e4e343e88511c50a7edc272fc2b07e
Doseok Jang <djang@ln002.brc>
2022-10-11 11:18:10 -0700

    fix sbatch script for wandb sweeps

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=fc_ntugame
 #
 # Partition:
-#SBATCH --partition=savio3_gpu
+#SBATCH --partition=savio2_1080ti
 #
 # Number of nodes:
 #SBATCH --nodes=1
@@ -15,10 +15,10 @@
 #SBATCH --ntasks=1
 #
 # Processors per task (please always specify the total number of processors twice the number of GPUs):
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #
 #Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
-#SBATCH --gres=gpu:GTX2080TI:2
+#SBATCH --gres=gpu:1
 #
 # Wall clock limit (8hrs):
 #SBATCH --time=3:00:00
@@ -29,7 +29,9 @@
 export WANDB_API_KEY=87928bf7ce62528545fe624701ab2f3aa25a7547
 export BAD_ROBOTS_PYTHON=/global/home/users/$USER/.conda/envs/ActiveRL/bin/python
 export WANDB_CACHE_DIR=/global/scratch/users/$USER/.cache/wandb
+export PYTHON_PATH=/global/home/users/$USER/.conda/envs/ActiveRL/bin/python
 # export LD_LIBRARY_PATH=/global/home/users/lucas_spangher/.conda/pkgs:$LD_LIBRARY_PATH
 module load gcc/8.3.0
- 
-wandb agent social-game-rl/active-rl/$1 --count 1
+export PATH=$PYTHON_PATH:$PATH
+source activate ActiveRL
+$PYTHON_PATH -m wandb agent social-game-rl/active-rl/$1 --count 1
