9e4d1c5f3e32d56f696d7a17b05387e0a60e57bf
Demosthen <1008981@gmail.com>
2022-10-31 17:12:13 -0700

    maze renders now

--- dm_maze/dm_maze.py
+++ dm_maze/dm_maze.py
@@ -1,22 +1,45 @@
-from dm_control.locomotion.tasks.random_goal_maze import RepeatSingleGoalMazeAugmentedWithTargets, DEFAULT_CONTROL_TIMESTEP, DEFAULT_PHYSICS_TIMESTEP, DEFAULT_ALIVE_THRESHOLD
+from dm_control.locomotion.tasks.random_goal_maze import RepeatSingleGoalMazeAugmentedWithTargets, RepeatSingleGoalMaze, DEFAULT_CONTROL_TIMESTEP, DEFAULT_PHYSICS_TIMESTEP, DEFAULT_ALIVE_THRESHOLD
 from dm_control.locomotion.props import target_sphere
 from dm_control.composer.observation import observable as observable_lib
 import labmaze
 import numpy as np
+from dm_control.locomotion.arenas import covering, mazes as maze_arenas
+
+_WALL_GEOM_GROUP = 3
 
 def str_maze_to_one_hot_maze(entity_layer):
     TOKENS = ['.', '+', labmaze.defaults.OBJECT_TOKEN, labmaze.defaults.SPAWN_TOKEN]
     TOKEN_DICT = {i: token for i, token in enumerate(TOKENS)}
     entity_ordinal = np.zeros(entity_layer.shape)
-    out_shape = list(entity_layer.shape) + len(TOKENS)
+    out_shape = list(entity_layer.shape) + [len(TOKENS)]
     out = np.zeros(out_shape)
     for i, token in enumerate(TOKENS):
-        out[entity_layer == token][:,:, i] = 1
+        out[entity_layer == token][:, i] = 1
     return out 
-    
-    
 
-class DM_Maze(RepeatSingleGoalMazeAugmentedWithTargets):
+class DM_Maze_Arena(maze_arenas.MazeWithTargets):
+    def _make_wall_geoms(self, wall_char):
+        i = 0
+        for x in range(len(self._maze.entity_layer)):
+            for y in range(len(self._maze.entity_layer[0])):
+                wall_mid = covering.GridCoordinates(
+                    (x + 0.5),
+                    (y + 0.5))
+                curr_char = self._maze.entity_layer[x, y]
+                is_wall = int(curr_char == wall_char)
+                wall_pos = np.array([(wall_mid.x - self._x_offset) * self._xy_scale,
+                                    -(wall_mid.y - self._y_offset) * self._xy_scale,
+                                    is_wall * self._z_height / 2 + 0.001])
+                wall_size = np.array([self._xy_scale / 2,
+                                        self._xy_scale / 2,
+                                        is_wall * self._z_height / 2 + 0.001])
+                self._maze_body.add('geom', name='wall{}_{}'.format(wall_char, i),
+                                    type='box', pos=wall_pos, size=wall_size,
+                                    group=_WALL_GEOM_GROUP)
+                self._make_wall_texturing_planes(wall_char, i, wall_pos, wall_size)
+                i += 1
+
+class DM_Maze_Task(RepeatSingleGoalMazeAugmentedWithTargets):
   """Augments the single goal maze with many lower reward targets."""
 
   def __init__(self,
@@ -34,23 +57,22 @@ class DM_Maze(RepeatSingleGoalMazeAugmentedWithTargets):
                aliveness_threshold=DEFAULT_ALIVE_THRESHOLD,
                contact_termination=True,
                physics_timestep=DEFAULT_PHYSICS_TIMESTEP,
-               control_timestep=DEFAULT_CONTROL_TIMESTEP):
-    super().__init__(
-        walker,
-        main_target,
-        maze_arena,
-        num_subtargets,
-        target_reward_scale,
-        subtarget_reward_scale,
-        subtarget_colors,
-        randomize_spawn_position,
-        randomize_spawn_rotation,
-        rotation_bias_factor,
-        aliveness_reward,
-        aliveness_threshold,
-        contact_termination,
-        physics_timestep,
-        control_timestep)
+               control_timestep=DEFAULT_CONTROL_TIMESTEP,
+               enable_global_task_observables=False):
+    super(RepeatSingleGoalMazeAugmentedWithTargets, self).__init__(
+        walker=walker,
+        target=main_target,
+        maze_arena=maze_arena,
+        target_reward_scale=target_reward_scale,
+        randomize_spawn_position=randomize_spawn_position,
+        randomize_spawn_rotation=randomize_spawn_rotation,
+        rotation_bias_factor=rotation_bias_factor,
+        aliveness_reward=aliveness_reward,
+        aliveness_threshold=aliveness_threshold,
+        contact_termination=contact_termination,
+        physics_timestep=physics_timestep,
+        control_timestep=control_timestep,
+        enable_global_task_observables=enable_global_task_observables)
     self._subtarget_reward_scale = subtarget_reward_scale
     self._subtargets = []
     for i in range(num_subtargets):
@@ -62,7 +84,13 @@ class DM_Maze(RepeatSingleGoalMazeAugmentedWithTargets):
       self._maze_arena.attach(subtarget)
     self._subtarget_rewarded = None
 
+    if enable_global_task_observables:
     # Reveal maze text map as observable.
-    maze_obs = observable_lib.Generic(
-        lambda _: self._maze_arena.maze.entity_layer)
-    maze_obs.enabled = True
+        maze_obs = observable_lib.Generic(
+            lambda _: str_maze_to_one_hot_maze(self._maze_arena.maze.entity_layer))
+        maze_obs.enabled = True
+        old_str_maze_obs = self._task_observables['maze_layout']
+        old_str_maze_obs.enabled = False
+        self._task_observables['maze_layout'] = maze_obs
+
+
--- dm_maze/dm_wrapper.py
+++ dm_maze/dm_wrapper.py
@@ -1,20 +1,30 @@
 from copy import copy
+from typing import Any
 from gym import spaces
 
 from dm_control import suite
 from dm_env import specs
 from dm2gym.envs.dm_suite_env import DMSuiteEnv
+import numpy as np
 
 def convert_dm_control_to_gym_space(dm_control_space):
     r"""Convert dm_control space to gym space. """
     if isinstance(dm_control_space, specs.BoundedArray):
-        space = spaces.Box(low=dm_control_space.minimum,
-                           high=dm_control_space.maximum, 
+        if np.ndim(dm_control_space.minimum) == 0:
+            dm_min = np.full(dm_control_space.shape, dm_control_space.minimum)
+        else:
+            dm_min = dm_control_space.minimum
+        if np.ndim(dm_control_space.maximum) == 0:
+            dm_max = np.full(dm_control_space.shape, dm_control_space.maximum)
+        else:
+            dm_max = dm_control_space.maximum
+        space = spaces.Box(low=dm_min,
+                           high=dm_max, 
                            dtype=dm_control_space.dtype,
                            shape=dm_control_space.shape)
-        
         assert space.shape == dm_control_space.shape
         return space
+
     elif isinstance(dm_control_space, specs.Array) and not isinstance(dm_control_space, specs.BoundedArray):
         space = spaces.Box(low=-float('inf'), 
                            high=float('inf'), 
@@ -38,3 +48,6 @@ class DMEnvWrapper(DMSuiteEnv):
         self.observation_space = convert_dm_control_to_gym_space(self.env.observation_spec())
         self.action_space = convert_dm_control_to_gym_space(self.env.action_spec())
         self.viewer = None
+
+    def __getattr__(self, __name: str) -> Any:
+        return getattr(self.env, __name)
